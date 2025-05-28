31f0ead7bcb0ea9f9f4036c8687271a1c85fcfae
AYOPELUMI <ayopelumi2014@gmail.com>
2025-05-28 04:37:30 -0700

    first commit

---
+++ src/app/companions/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CompanionSession
+    = () => {
+        return (
+            <div>CompanionSession
+
+            </div>
+        )
+    }
+
+export default CompanionSession
